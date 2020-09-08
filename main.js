6236923bb7d7baace2e8864ea6dd64a7ab34c0db
j3ffh95 <j3ffh95@gmail.com>
2020-09-07 23:42:16 -0400

    Added visual and colors array

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+
+  const color = ["60d394", "db3752", "9127c2", "6092d3", "d39860", "caeb52"];
 
   pads.forEach((pad, index) => {
     pad.addEventListener("click", function () {
